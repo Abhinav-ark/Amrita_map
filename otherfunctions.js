34e87e2e37b0ff8b75ac2c2026ced76c63b94692
Abhinav-ark <76396917+Abhinav-ark@users.noreply.github.com>
2023-06-09 18:32:17 +0530

    Update otherfunctions.js

--- otherfunctions.js
+++ otherfunctions.js
@@ -5,3 +5,15 @@ function swapValues(){
     start.value = end.value;
     end.value = temp;
 }
+function clearSearch(){
+    document.getElementById("start").value=null;
+    document.getElementById("end").value=null;
+}
+
+function originalPos(){
+    target.style.transform = 'rotate(0deg)';
+    target.style.top=0;
+    target.style.left=0;
+    target.style.height='35em';
+    target.style.width='19em';
+}
