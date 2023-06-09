b6296d6d0ea27210b5f5173f63357cda6649da76
Abhinav-ark <76396917+Abhinav-ark@users.noreply.github.com>
2023-06-10 00:40:57 +0530

    Add files via upload

---
+++ otherfunctions.js
@@ -0,0 +1,23 @@
+function swapValues(){
+    var start = document.getElementById("start");
+    var end = document.getElementById("end");
+    var temp = start.value;
+    start.value = end.value;
+    end.value = temp;
+}
+function clearSearch(){
+    document.getElementById("start").value=null;
+    document.getElementById("end").value=null;
+}
+
+function originalPos(){
+    target.style.transform = 'rotate(0deg)';
+    target.style.top=0;
+    target.style.left=0;
+    target.style.height='35em';
+    target.style.width='19em';
+}
+
+function mapNavInfo(){
+    alert("On a TouchSceen, Use 1 finger to Pan, 2 fingers to zoom and 3 fingers to rotate.");
+}
