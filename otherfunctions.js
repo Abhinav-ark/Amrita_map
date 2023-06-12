6dd763534996096d0742992e66da62b7d626434e
Abhinav-ark <76396917+Abhinav-ark@users.noreply.github.com>
2023-06-13 03:04:25 +0530

    Delete otherfunctions.js

--- otherfunctions.js
+++
@@ -1,7 +0,0 @@
-function swapValues(){
-    var start = document.getElementById("start");
-    var end = document.getElementById("end");
-    var temp = start.value;
-    start.value = end.value;
-    end.value = temp;
-}
