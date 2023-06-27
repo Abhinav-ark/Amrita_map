f357f49f99a653f10628dd24bda8d0f5e17ac2e8
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2023-06-27 06:18:42 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,7 +1,9 @@
 const sandwich = document.querySelector('.sandwich');
 const navbar = document.querySelector('.mobile-nav');;
+const scroller = document.querySelector('.scroller');
 
 sandwich.addEventListener('click', function() {
     sandwich.classList.toggle('is-active');
     navbar.classList.toggle('is-active');
+    scroller.classList.toggle('fixed');
 });
